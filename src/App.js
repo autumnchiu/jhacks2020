26ee51d4bda79899b58124756a87b730f7659d40
Autumn Chiu <autumnchiu@gmail.com>
2020-02-16 08:28:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,33 +23,6 @@ class App extends React.Component {
     this.takenCallBack = this.takenCallBack.bind(this)
     
   }
-  /*const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(42068);
-  const [user, setUsers] = useState([
-    {name: 'John', message: "I am john snow"},
-    {name: "Aidan", message: "I am a nerdddddd"}
-  ]);
-  const increment = () =>{
-    setCount(count+1);
-    setRed(!isRed);
-  }
-  return(
-    <div>
-      {user.map(user =>(
-        <Tweet name ={user.name} message = {user.message}/>
-      ))}
-   
-      <h1> Hello Eric u dumb</h1>
-      <Hello />
-      {/* <Tweet name = "samson" />
-      <Tweet name = "eric"/>
-      <Tweet name = "dumdumAidan"/> */
-/*<button onClick = {increment}>Increment</button>
-      <h1>{count}</h1>
-      <h1 className = {isRed ? 'red': ""}> change my color</h1> 
-    }
-    </div>
-  );*/
 
   mainCallBack = (childData) => {
     this.setState({ mainData: childData, onMain: false, onTaken: true });
